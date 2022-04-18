a98dcf88bfc0135218b573961c36b8e78fe71b22
insidelamp <rkdhfh@naver.com>
2022-04-18 22:19:33 +0900

    중요?

--- clone/src/App.js
+++ clone/src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from "react";
 import { Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import UserProfile from "./pages/UserProfile";
 import { history } from "./redux/configstore";
 import { ConnectedRouter } from "connected-react-router";
-import './App.css';
-import DirectMessage from './pages/DirectMessage';
-import ChatDetaill from './pages/ChatDetaill';
+import "./App.css";
+import DirectMessage from "./pages/DirectMessage";
+import ChatDetaill from "./pages/ChatDetaill";
 //import test from './test';
-import Iinvitation from './chating/Iinvitation';
-
-
+import Iinvitation from "./chating/Iinvitation";
 
 function App() {
   return (
@@ -20,13 +18,12 @@ function App() {
         <Route path="/" exact component={Login} />
         <Route path="/signup" exact component={Signup} />
         <Route path="/user/:id" exact component={UserProfile} />
-          <Route path="/dm" exact component={DirectMessage} />
-          <Route path="/chat/:roomid" exact component={ChatDetaill} />
-          {/* <Route path="/test" exact component={test} /> */}
-          <Route path="/Iinvitation" exact component={Iinvitation} />
+        <Route path="/dm" exact component={DirectMessage} />
+        <Route path="/chat/:roomid" exact component={ChatDetaill} />
+        {/* <Route path="/test" exact component={test} /> */}
+        <Route path="/invitation" exact component={Iinvitation} />
       </ConnectedRouter>
     </React.Fragment>
-
   );
 }
 
